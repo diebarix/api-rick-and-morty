5a5989173b190ee0515d671c068acd21d9dd736d
Diebarix <diegoabr0704@gmail.com>
2022-07-24 19:44:05 -0500

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://diebarix.github.io/api-rick-and-morty/",
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: "/api-rick-and-morty/",
+});
